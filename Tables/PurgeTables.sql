d421b96f6aabd4fdd498aeab344252667dbebd54
Luke MacDonald <lukemac82@gmail.com>
2020-02-04 20:46:08 -0500

    Commiting master with new table to repo

---
+++ Tables/PurgeTables.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PurgeTables]
+(
+	[TableName] VARCHAR(50) NOT NULL CONSTRAINT PK_TableName PRIMARY KEY,
+	[PurgeByColumn] VARCHAR(50) NOT NULL,
+	[Retention]	INT NOT NULL,
+	[BatchSize] INT NOT NULL
+)
