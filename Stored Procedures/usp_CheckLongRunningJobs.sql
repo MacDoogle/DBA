a18b1b9a98497f05018330342bcc7732f7880e54
Luke MacDonald <lmacdonald@inntopia.com>
2020-03-17 09:12:02 -0400

    Added usp_CheckLongRunningJobs

---
+++ Stored Procedures/usp_CheckLongRunningJobs.sql
@@ -0,0 +1,66 @@
+﻿USE [Inntopia2]
+GO
+
+/****** Object:  StoredProcedure [dbo].[ipDBA_CheckForHangingSQLAgentJobs]    Script Date: 3/17/2020 9:06:29 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[usp_CheckLongRunningJobs]
+-- exec ipDBA_CheckForHangingSQLAgentJobs
+AS
+BEGIN
+	DECLARE @jobname VARCHAR(400),
+		@jobstart	 VARCHAR(100),
+		@message	 VARCHAR(4000) = '',
+		@EmailDBA	 VARCHAR(MAX)  = 'lukemac82@gmail.com';
+
+	DECLARE jobs CURSOR FOR
+		SELECT j.name,
+			ja.start_execution_date
+		FROM msdb.dbo.sysjobactivity ja
+			INNER JOIN msdb.dbo.sysjobs j ON j.job_id = ja.job_id
+		WHERE ja.start_execution_date <= DATEADD(mi, -180, GETDATE())
+			AND ja.stop_execution_date IS NULL
+			AND j.name != 'RSys Distribution Web Service Client - MOUNTAINRES'
+			-- Only get the jobs running in the active session.
+			AND ja.session_id = (SELECT MAX(session_id) from msdb.dbo.syssessions)
+			-- cdc.Inntopia2_capture is always running, so ignore that.
+			AND j.name != 'cdc.Inntopia2_capture';
+
+	OPEN jobs;
+
+	FETCH NEXT FROM jobs
+	INTO @jobname,
+		@jobstart;
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		SET @message = @message + @jobname + ' started on ' + @jobstart + CHAR(13);
+
+		FETCH NEXT FROM jobs
+		INTO @jobname,
+			@jobstart;
+	END;
+
+	CLOSE jobs;
+	DEALLOCATE jobs;
+
+	IF @message IS NOT NULL AND @message != ''
+	BEGIN
+		DECLARE @msgSubject varchar(100)
+		SET @msgSubject = 'Check Hanging SQL Agent Jobs - ' + @@SERVERNAME
+		EXEC dbo.ipEmailSend_v2 @MailTo = @EmailDBA,
+			@Subject = @msgSubject,
+			@MailFromDisplay = 'DBAs',
+			@MailFrom = @EmailDBA,
+			@fText = 1,
+			@Message = @message;
+	END;
+END;
+GO
+
+
